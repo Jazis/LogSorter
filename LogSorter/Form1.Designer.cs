ab98bcad8cd90f0b1a1762742b19dc8347d01e3d
unknown <gulko46@gmail.com>
2021-07-11 12:07:18 +0300

    New creation

---
+++ LogSorter/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace LogSorter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(466, 164);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(12, 182);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(466, 164);
+            this.listBox2.TabIndex = 2;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(484, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(513, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(484, 40);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(513, 22);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(484, 68);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(513, 22);
+            this.textBox3.TabIndex = 6;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(484, 96);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(513, 250);
+            this.richTextBox1.TabIndex = 7;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(965, 352);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(32, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "?";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1009, 375);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "LogSorter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ LogSorter/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+
+namespace LogSorter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (string folder in Directory.GetDirectories(Directory.GetCurrentDirectory()))
+            {
+                listBox1.Items.Add(folder.Split('\\')[folder.Split('\\').Length - 1]);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                foreach (string line in File.ReadLines(listBox1.SelectedItem.ToString() + "\\Passwords.txt"))
+                {
+                    if (line.Contains("http") & !line.Contains("REDLINESUPPORT"))
+                    {
+                        listBox2.Items.Add(line);
+                    }
+                }
+            }
+            catch { };
+        }
+
+        public void cookieManage(string line)
+        {
+            string[] stringSeparators = new string[] { "	" };
+            var lll = line.Split(stringSeparators, 7, StringSplitOptions.None);
+            //richTextBox1.Text += "\n    {\n";
+            //richTextBox1.Text += "        \"name\": \"" + lll[5] + "\",\n";
+            //richTextBox1.Text += "        \"value\": \"" + lll[6] + "\",\n";
+            //richTextBox1.Text += "        \"domain\": \"" + lll[0] + "\",\n";
+            //richTextBox1.Text += "        \"hostonly\": " + lll[1] + ",\n";
+            //richTextBox1.Text += "        \"path\": \"" + lll[2] + "\",\n";
+            //richTextBox1.Text += "        \"secure\": " + lll[3] + ",\n";
+            //richTextBox1.Text += "        \"httpOnly\": " + lll[3] + ",\n";
+            //richTextBox1.Text += "        \"sameSite\": \"no_restriction\",\n";
+            //richTextBox1.Text += "        \"session\": true,\n";
+            //richTextBox1.Text += "        \"firstPartyDomain\": \"\",\n";
+            //richTextBox1.Text += "        \"expirationDate\": \"" + lll[4] + "\",\n";
+            //richTextBox1.Text += "        \"storeId\": null\n";
+            //richTextBox1.Text += "    },\n";
+            //richTextBox1.Text.Replace("},", "},\n{");
+
+            richTextBox1.Text += "{";
+            richTextBox1.Text += "\"domain\": \"" + lll[0] + "\",";
+            richTextBox1.Text += "\"expirationDate\": " + lll[4] + ",";
+            richTextBox1.Text += "\"httpOnly\": " + lll[3].ToLower() + ",";
+            richTextBox1.Text += "\"name\": \"" + lll[5] + "\",";
+            richTextBox1.Text += "\"path\": \"" + lll[2] + "\",";
+            richTextBox1.Text += "\"secure\": " + lll[3].ToLower() + ",";
+            richTextBox1.Text += "\"value\": \"" + lll[6] + "\",";
+            richTextBox1.Text += "\"hostonly\": " + lll[1].ToLower();
+            richTextBox1.Text += "},";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                richTextBox1.Text = string.Empty;
+                richTextBox1.Text += "[";
+                string newLink = listBox2.SelectedItem.ToString().Split('/')[2];
+                foreach (string file in Directory.GetFiles(Directory.GetCurrentDirectory() + "\\" + listBox1.SelectedItem.ToString() + "\\Cookies\\"))
+                {
+                    foreach (string line in File.ReadLines(file))
+                    {
+                        if (line.Contains(newLink))
+                        {
+                            cookieManage(line);
+                        }
+                    }
+                }
+                richTextBox1.Text = richTextBox1.Text.Substring(0, richTextBox1.Text.Length - 2);
+                richTextBox1.Text += "}]";
+            }
+            catch { richTextBox1.Text = string.Empty; }
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = string.Empty;
+            textBox2.Text = string.Empty;
+            textBox3.Text = string.Empty;
+            richTextBox1.Text = string.Empty;
+            int c0 = 0;
+            using (StreamReader streamReader = new StreamReader(listBox1.SelectedItem.ToString() + "\\Passwords.txt", Encoding.Default))
+            {
+                var fileLine = File.ReadAllLines(listBox1.SelectedItem.ToString() + "\\Passwords.txt");
+                for (int i = 0; i <= fileLine.Length; i++)
+                {
+                    try
+                    {
+                        if (fileLine[i].Contains(listBox2.SelectedItem.ToString()))
+                        {
+                            textBox1.Text += fileLine[i].Replace("URL: ", string.Empty) + "\n";
+                            textBox2.Text += fileLine[i + 1].Replace("Username: ", string.Empty) + "\n";
+                            textBox3.Text += fileLine[i + 2].Replace("Password: ", string.Empty) + "\n";
+                        }
+                    }
+                    catch { }
+                }
+            }
+            try
+            {
+                richTextBox1.Text += "[";
+                string newLink = listBox2.SelectedItem.ToString().Split('/')[2];
+                foreach (string file in Directory.GetFiles(Directory.GetCurrentDirectory() + "\\" + listBox1.SelectedItem.ToString() + "\\Cookies\\"))
+                {
+                    foreach (string line in File.ReadLines(file))
+                    {
+                        if (line.Contains(newLink))
+                        {
+                            cookieManage(line);
+                        }
+                    }
+                }
+                richTextBox1.Text = richTextBox1.Text.Substring(0, richTextBox1.Text.Length - 2);
+                richTextBox1.Text += "\n}]";
+            }
+            catch { richTextBox1.Text = string.Empty; richTextBox1.Text = "NO COOKIES"; }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            textBox1.Text = "https://addons.mozilla.org/ru/firefox/addon/cookie-editor/?utm_source=addons.mozilla.org&utm_medium=referral&utm_content=search";
+            textBox2.Text = "https://chrome.google.com/webstore/detail/cookie-editor/hlkenndednhfkekhgcdicdfddnkalmdm";
+            textBox3.Text = "Created by Jazis";
+        }
+    }
+}
